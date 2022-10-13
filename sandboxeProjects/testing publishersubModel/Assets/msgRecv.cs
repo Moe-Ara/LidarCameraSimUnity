ea997f6c86a6feb3be6040e7add6daeddd9d11bd
Moe-Ara <hulk78901@gmail.com>
2022-10-13 20:01:19 +0200

    nothing

---
+++ sandboxeProjects/testing publishersubModel/Assets/msgRecv.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace com
+{   
+    [Serializable]
+    public class msgRecv
+    {
+        public float speed = 0;
+
+        public float yaw = 0;
+        public override string ToString()
+        {
+            return "speed is: " + speed + " and yaw is: " + yaw;
+        }
+    }
+}
