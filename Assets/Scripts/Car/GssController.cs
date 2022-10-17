6b56de10782b72d4a8b94dc09b66c54958617e07
Moe-ara <mohamadarabi@web.de>
2022-10-17 16:26:21 +0200

    gss+Error

--- Assets/Scripts/Car/GssController.cs
+++ Assets/Scripts/Car/GssController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using Communication.Messages;
 using UnityEngine;
 using UnityEngine.UIElements;
-
+	
 namespace Car.gss
 {
     public class GssController : MonoBehaviour
@@ -19,7 +19,10 @@ namespace Car.gss
          */
         public float _error;
         private Vector3 _lastPositon= Vector3.zero;
-        private float _speed=0.0f;
+        public float _speed=0.0f;
+		public double _speedKmH=0;
+		private float _offset=0.0f;
+		public double _time=0;
         // Start is called before the first frame update
         void Start()
         {
@@ -29,6 +32,7 @@ namespace Car.gss
         // Update is called once per frame
         void FixedUpdate()
         {
+				calculateSpeed();
 
         }
 
@@ -36,16 +40,39 @@ namespace Car.gss
         {
             Vector3 pos = transform.position;
             Vector3 difference = (pos - _lastPositon) / Time.deltaTime;
+			_time = Time.deltaTime;
             _speed = difference.magnitude;
             _lastPositon = pos;
-            return _speed;
+			_speedKmH= _speed * 3.6;
+			addError();
+            return _speed+_offset; // multiply by random for error calculation 
         }
+		public void addError(){
+			if (_speed<3.5f){
+				_speed= calculateErrorGuassian(_speed, (float)(Math.Sqrt(0.03 * _speed)));
+			}
+			else if (_speed > 3.5){
+				_speed= calculateErrorGuassian(_speed, (float)(Math.Sqrt(0.01 * _speed)));		
+			}
+			//_speed= _speed+_error;
 
-        public float calculateError(ControlResultMessage msg)
-        {
-            _error = (Math.Abs((msg.speed_target - _speed)) / _speed) * 100;
-            return _error;
-        }
+}
+    public float calculateErrorGuassian(float mean, float stddev){
+			System.Random random=new System.Random();
+            // The method requires sampling from a uniform random of (0,1]
+            // but Random.NextDouble() returns a sample of [0,1).
+            float x1 = 1 - (float)random.NextDouble();
+            float x2 = 1 - (float)random.NextDouble();
+
+            float y1 = (float)(Math.Sqrt(-2.0 * Math.Log(x1)) * Math.Cos(2.0 * Math.PI * x2));
+            return y1 * stddev + mean;
+}
+		
+        //public float calculateError(ControlResultMessage msg)
+        //{
+         //   _error = (Math.Abs((msg.speed_target - _speed)) / _speed) * 100;
+           // return _error;
+        //}
     }
 
 }
