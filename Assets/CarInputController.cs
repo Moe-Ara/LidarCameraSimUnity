8627d37c9d91d4a7f9a15dcd471a70269418e694
Moe-ara <mohamadarabi@web.de>
2022-10-24 21:49:52 +0200

    work on manual control of the car using user input

--- Assets/CarInputController.cs
+++ Assets/CarInputController.cs
@@ -1,12 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 /// <summary>
 /// This class is responsible for controlling the car with the WASD or Gamepad Controls.
 /// </summary>
 public class CarInputController : MonoBehaviour
 {
+    private Vector2 _moveDirection;
+
+    public void Start()
+    {_moveDirection=Vector2.zero;
+        
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _moveDirection = context.ReadValue<Vector2>();
+        
+        // transform.Translate(new Vector3(_moveDirection.x, 0, _moveDirection.y) * 15f * Time.deltaTime);
+        MoveCar(_moveDirection.x, _moveDirection.y);
+        Debug.Log(_moveDirection.x + " " + _moveDirection.y );
+    }
+
+    private void MoveCar(float x, float y)
+    {
+        var speed = 15f;
+        rearLeftCollider.motorTorque = speed * 200.0f ;
+        rearRightCollider.motorTorque = speed * 200.0f ;
+        frontLeftCollider.steerAngle = y * 45.0f;
+        frontRightCollider.steerAngle = y * 45.0f;
+    }
     /// <summary>
     /// The colliders of the front wheels (for steering)
     /// </summary>
@@ -25,23 +51,26 @@ public class CarInputController : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
-        // Get Steering
-        var steering = Input.GetAxis("Horizontal");
-        var gas = Input.GetAxis("Vertical");
-        // Get Gas
-
-        // Apply forces in NM 
-        rearLeftCollider.motorTorque = gas * 200.0f;
-        rearRightCollider.motorTorque = gas * 200.0f;
-
-        // Steering
-        frontLeftCollider.steerAngle = steering * 45.0f;
-        frontRightCollider.steerAngle = steering * 45.0f;
-
-        // Update Transforms
-        //frontLeftTransform.rotation = frontLeftCollider.transform.rotation;
-        //frontRightTransform.rotation = frontRightCollider.transform.rotation;
-        //rearLeftTransform.rotation = rearLeftCollider.transform.rotation;
-        //rearRightTransform.rotation = rearRightTransform.transform.rotation;
+        // MoveCar(_moveDirection.x, _moveDirection.y);
+        // // Get Steering
+        // var steering = Input.GetAxis("Horizontal");
+        // var gas = Input.GetAxis("Vertical");
+        // // Get Gas
+        //
+        // // Apply forces in NM 
+        // rearLeftCollider.motorTorque = gas * 200.0f;
+        // rearRightCollider.motorTorque = gas * 200.0f;
+        //
+        // // Steering
+        // frontLeftCollider.steerAngle = steering * 45.0f;
+        // frontRightCollider.steerAngle = steering * 45.0f;
+        //
+        // // Update Transforms
+        // //frontLeftTransform.rotation = frontLeftCollider.transform.rotation;
+        // //frontRightTransform.rotation = frontRightCollider.transform.rotation;
+        // //rearLeftTransform.rotation = rearLeftCollider.transform.rotation;
+        // //rearRightTransform.rotation = rearRightTransform.transform.rotation;
     }
+
+
 }
