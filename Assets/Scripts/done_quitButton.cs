165527ece7b5feb50f25e4d9c2c06f8f4d7c8661
lin <crystallin20000205@gmail.com>
2022-10-28 10:29:08 +0200

    GUI branch

---
+++ Assets/Scripts/done_quitButton.cs
@@ -0,0 +1,26 @@
+/*
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class done_quitButton : MonoBehaviour
+{
+    [Header("UI")] 
+    public InputField massInput;
+    public InputField actualspeedInput;
+    public InputField GSSInput;
+    public InputField IMUInput;
+    
+    public void doneButton()
+    {
+        var request = new 
+    }
+
+    public void quitButton()
+    {
+        
+    }
+}
+*/
