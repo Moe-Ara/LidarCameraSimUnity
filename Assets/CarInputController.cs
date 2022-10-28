92a64b68074695b973eef6f7055d0f1c0088c0d7
Moe-Ara <mohamadarabi@web.de>
2022-10-28 17:50:27 +0200

    Car input Controller Works

--- Assets/CarInputController.cs
+++ Assets/CarInputController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 // using UnityEngine.InputSystem;
 
 /// <summary>
@@ -12,27 +14,15 @@ public class CarInputController : MonoBehaviour
     private Vector2 _moveDirection;
 
     public void Start()
-    {_moveDirection=Vector2.zero;
-        
-    }
-
-    // public void OnMove(InputAction.CallbackContext context)
-    // {
-    //     _moveDirection = context.ReadValue<Vector2>();
-    //     
-    //     // transform.Translate(new Vector3(_moveDirection.x, 0, _moveDirection.y) * 15f * Time.deltaTime);
-    //     MoveCar(_moveDirection.x, _moveDirection.y);
-    //     Debug.Log(_moveDirection.x + " " + _moveDirection.y );
-    // }
-
-    private void MoveCar(float x, float y)
     {
-        var speed = 15f;
-        rearLeftCollider.motorTorque = speed * 200.0f ;
-        rearRightCollider.motorTorque = speed * 200.0f ;
-        frontLeftCollider.steerAngle = y * 45.0f;
-        frontRightCollider.steerAngle = y * 45.0f;
+        _moveDirection = Vector2.zero;
     }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _moveDirection = context.ReadValue<Vector2>() * new Vector2(1, 10);
+    }
+
     /// <summary>
     /// The colliders of the front wheels (for steering)
     /// </summary>
@@ -51,26 +41,10 @@ public class CarInputController : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
-        // MoveCar(_moveDirection.x, _moveDirection.y);
-        // // Get Steering
-        // var steering = Input.GetAxis("Horizontal");
-        // var gas = Input.GetAxis("Vertical");
-        // // Get Gas
-        //
-        // // Apply forces in NM 
-        // rearLeftCollider.motorTorque = gas * 200.0f;
-        // rearRightCollider.motorTorque = gas * 200.0f;
-        //
-        // // Steering
-        // frontLeftCollider.steerAngle = steering * 45.0f;
-        // frontRightCollider.steerAngle = steering * 45.0f;
-        //
-        // // Update Transforms
-        // //frontLeftTransform.rotation = frontLeftCollider.transform.rotation;
-        // //frontRightTransform.rotation = frontRightCollider.transform.rotation;
-        // //rearLeftTransform.rotation = rearLeftCollider.transform.rotation;
-        // //rearRightTransform.rotation = rearRightTransform.transform.rotation;
+        // Move using Wheel Collider
+        rearLeftCollider.motorTorque = 10.0f * _moveDirection.y;
+        rearRightCollider.motorTorque = 10.0f * _moveDirection.y;
+        frontLeftCollider.steerAngle = 45.0f * _moveDirection.x;
+        frontRightCollider.steerAngle = 45.0f * _moveDirection.x;
     }
-
-
 }
