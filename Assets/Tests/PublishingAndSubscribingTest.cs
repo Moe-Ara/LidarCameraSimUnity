da5474661fb5d301c67b28ee4d121a921b7aca2d
Moe-Ara <mohamadarabi@web.de>
2022-11-10 02:53:47 +0100

    Cleaned up; added comments; refactored and fixed bugs

--- Assets/Tests/PublishingAndSubscribingTest.cs
+++ Assets/Tests/PublishingAndSubscribingTest.cs
@@ -2,40 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using Car.Camera;
+using Car.Lidar;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 using Communication;
 using UnityEditor;
 
+
 public class PublishingAndSubscribingTest
 {
+    
+    /// <summary>
+    /// Class for test cases
+    /// </summary>
+    
     //~Camera
     private Publisher CamPub;
     private Subscriber CamSub;
-    
+
     //~ Reusables
     private GameObject car;
-    private bool data;
+    private bool Camdata;
+    private bool LidData;
     private byte[] msg;
 
-    
+    /// <summary>
+    /// Set up
+    /// </summary>
     [SetUp]
     public void Setup()
     {
+        car = GameObject.Instantiate(Resources.Load<GameObject>("Car"));
         {
             //~ CAMERA DATA TEST
-            car = GameObject.Instantiate(Resources.Load<GameObject>("Car"));
             var _carCamCont =car.GetComponent<CarCameraController>();
-            data = false;
+            Camdata = false;
+
             CamPub = new Publisher(42000);
             _carCamCont.OnNewImage = msg => CamPub.Publish(msg);
             msg = Encoding.UTF8.GetBytes("Hello I am Publisher");
-            CamSub = new Subscriber(42000, onNewData);
+            CamSub = new Subscriber(42000, onNewCamData);
         }
-        
     }
-    
+    /// <summary>
+    /// testing if the camera publisher and subscriber are working 
+    /// </summary>
     [UnityTest]
     public IEnumerator CameraData()
     {  
@@ -43,12 +55,14 @@ public class PublishingAndSubscribingTest
         // Use yield to skip a frame.
         CamPub.Publish(msg);
         yield return new WaitForSeconds(0.3f);
-        Assert.IsTrue(data);
+        Assert.IsTrue(Camdata);
     }
+    
 
-    private void onNewData(byte[] d)
+    private void onNewCamData(byte[] d)
     {
         if(d!=null)
-            data = true;
+            Camdata = true;
     }
+
 }
